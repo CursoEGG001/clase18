6f73fca6b488772170286dae8359ffebe6a9179e
CursoEGG001 <soygecko@gmail.com>
2023-08-12 20:46:38 -0300

    Realizar un programa que elimine los dos últimos elementos de un array. Mostrar el resultado.
    Muestra del funcionamiento de los nodos con los resultados.

--- Ejercicio17/public_html/ejercicio17.js
+++ Ejercicio17/public_html/ejercicio17.js
@@ -3,9 +3,26 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
  */
 var arreglo1 = new Array(5);
-for (var i = 0; i < arreglo1.length; i++) {
-    arreglo1[i]= prompt(`Ingrese el dato ${i+1}:`);
-}
-window.alert("Los datos actuales: \n"+arreglo1.toString());
-window.alert("Los datos ahora: \n"+arreglo1.slice(0,3)); //el cuarto (posicion 3) no se incluye.
+elementos = document.getElementById("espacio");
+listado = document.createElement("ul");
+listado.setAttribute("class", "border mt-2");
+elementos.appendChild(listado);
 
+for (var i = 0; i < arreglo1.length; i++) {
+    arreglo1[i] = prompt(`Ingrese el dato ${i + 1}:`);
+
+    let info = document.createElement("li");
+    info.setAttribute("class", "fs-2");
+    info.setAttribute("id", `elItem_${arreglo1[i]}`);
+    info.innerHTML = arreglo1[i];
+    listado.appendChild(info);
+}
+console.log("Los datos actuales: \n" + arreglo1.toString());
+
+console.log("Los datos ahora: \n" + arreglo1.slice(0, 3)); //el cuarto (posicion 3) no se incluye.
+for (var dato in arreglo1) {
+    console.log(arreglo1.slice(0,3)[dato]);
+    elempag=document.getElementById(`elItem_${arreglo1.slice(0,3)[dato]}`);
+    elempag.setAttribute("class","fs-4");
+    elementos.appendChild(elempag);
+}
